b89752027fde2373850826ee2a343afd91f6300a
Dan Walker <dan@rowdypixel.com>
2015-05-18 17:44:19 -0400

    Genericize the calculation to allow for easier adding of different leanness

--- js/beef.js
+++ js/beef.js
@@ -1,29 +1,32 @@
 $(document).ready(function() {
 
   $('input#calculate-button').click(function() {
-      var leastLean = getRealPrice(73, $('#73-27-price').val())
-      var middleLean = getRealPrice(80, $('#80-20-price').val())
-      var mostLean = getRealPrice(85, $('#85-15-price').val())
 
-      var lowest = Math.min(leastLean, middleLean, mostLean);
+    var prices = [];
 
-      if(lowest == leastLean) {
-        $('#results').html('73/27');
+    $('input.beef-price').each(function() {
+      var leanPercent = $(this).attr('data-lean-percent');
+      var totalPrice = $(this).val();
+
+      if(totalPrice > 0) {
+        var value = getRealPrice(leanPercent, totalPrice);
+        prices.push({ percent: leanPercent, price: value});
       }
-      else if(lowest == middleLean) {
-          $('#results').html('80/20');
-        }
-      else if(lowest == mostLean) {
-          $('#results').html('85/15');
-      }
+    })
+
+    prices.sort(function(a,b) {
+       if(a.price < b.price) return -1;
+       if (a.price > b.price) return 1;
+       if(a.price == b.price) return 0;
+    });
+
+
+    $('#results').html(prices[0].percent + '% beef - $' + prices[0].price + '/lb');
   });
-
 
   function getRealPrice(percentLean, price) {
     var leanAsRealPercent = ( percentLean/ 100);
       var realPrice = price / leanAsRealPercent;
-
-      console.log(percentLean, '-', realPrice)
-      return realPrice;
+      return Math.round(realPrice * 100) / 100;
   }
 })
