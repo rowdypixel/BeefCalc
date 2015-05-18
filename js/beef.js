0616c8cbc11bdf196a6245fa943eb4e57da3aedc
Dan Walker <dan@rowdypixel.com>
2015-05-18 17:11:24 -0400

    Calculate real price/lb and display lowest

--- js/beef.js
+++ js/beef.js
@@ -2,22 +2,28 @@ $(document).ready(function() {
 
   $('input#calculate-button').click(function() {
       var leastLean = getRealPrice(73, $('#73-27-price').val())
-      var middleLean = getRealPrice(73, $('#80-20-price').val())
-      var mostLean = getRealPrice(73, $('#85-15-price').val())
+      var middleLean = getRealPrice(80, $('#80-20-price').val())
+      var mostLean = getRealPrice(85, $('#85-15-price').val())
 
-      var array = [leastLean, middleLean, mostLean];
-      array.sort();
+      var lowest = Math.min(leastLean, middleLean, mostLean);
 
-      if(array[0] == leastLean)
+      if(lowest == leastLean) {
         $('#results').html('73/27');
-      if(array[0] == middleLean)
+      }
+      else if(lowest == middleLean) {
           $('#results').html('80/20');
-      if(array[0] == mostLean)
+        }
+      else if(lowest == mostLean) {
           $('#results').html('85/15');
+      }
   });
 
 
   function getRealPrice(percentLean, price) {
+    var leanAsRealPercent = ( percentLean/ 100);
+      var realPrice = price / leanAsRealPercent;
 
+      console.log(percentLean, '-', realPrice)
+      return realPrice;
   }
 })
